4226acdb0f0d0cdb55610ecb6f9ed2f4564a976e
Yahya Ganjo <yahya.ganjo@gmail.com>
2022-07-03 14:06:13 +0200

    cors proxy

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -32,18 +32,15 @@ const Checkout = () => {
     };
 
     shippingInfo.push(userShippingAddress);
-    fetch(
-      'https://vast-beyond-42437.herokuapp.com/https://fast-ravine-20619.herokuapp.com/create-checkout-session',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          items: cartProducts,
-        }),
-      }
-    )
+    fetch('https://fast-ravine-20619.herokuapp.com/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        items: cartProducts,
+      }),
+    })
       .then((res) => {
         if (res.ok) return res.json();
         return res.json().then((json) => Promise.reject(json));
