afcdca4ee1837d77b5ee31384136d7d8817e3b13
Yahya Ganjo <yahya.ganjo@gmail.com>
2022-07-05 03:16:25 +0200

    remove card payment

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,55 +1,100 @@
-import React from 'react';
-/*import { useRef } from 'react';
+import React, { useRef, useState, useContext } from 'react';
 import Helmet from '../components/Helmet/Helmet';
 import CommonSection from '../components/UI/common-section/CommonSection';
 import { Container, Row, Col } from 'reactstrap';
-import { Link } from 'react-router-dom';*/
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../store/auth-context';
 
 const Login = () => {
-  /*const loginNameRef = useRef();
+  const loginNameRef = useRef();
   const loginPasswordRef = useRef();
+  const [isLoading, setIsLoading] = useState(false);
+  const authCtx = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const submitHandler = (e) => {
     e.preventDefault();
-  };*/
+
+    const enteredEmail = loginNameRef.current.value;
+    const enteredPassword = loginPasswordRef.current.value;
+
+    setIsLoading(true);
+
+    fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB47NsZujsnouqbKgpUrkER-LDp66QA8Pc',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Authenticatie mislukt!';
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        authCtx.login(data.idToken, expirationTime.toISOString());
+        navigate('/admin');
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
 
   return (
-    /*<Helmet title="Login">
-      <CommonSection title="Login" />
+    <Helmet title='Login'>
+      <CommonSection title='Login' />
       <section>
         <Container>
           <Row>
-            <Col lg="6" md="6" sm="12" className="m-auto text-center">
-              <form className="form mb-5" onSubmit={submitHandler}>
-                <div className="form__group">
+            <Col lg='6' md='6' sm='12' className='m-auto text-center'>
+              <form className='form mb-5' onSubmit={submitHandler}>
+                <div className='form__group'>
                   <input
-                    type="email"
-                    placeholder="Email"
+                    type='email'
+                    placeholder='Email'
                     required
                     ref={loginNameRef}
                   />
                 </div>
-                <div className="form__group">
+                <div className='form__group'>
                   <input
-                    type="password"
-                    placeholder="Password"
+                    type='password'
+                    placeholder='Password'
                     required
                     ref={loginPasswordRef}
                   />
                 </div>
-                <button type="submit" className="addTOCart__btn">
-                  Login
-                </button>
+                {!isLoading && (
+                  <button className='addTOCart__btn'>Inloggen</button>
+                )}
+                {isLoading && <p>Verzenden</p>}
               </form>
-              <Link to="/register">
-                Don't have an account? Create an account
-              </Link>
             </Col>
           </Row>
         </Container>
       </section>
-    </Helmet>*/
-    <h1>Deze Pagina Is In Onderhoud</h1>
+    </Helmet>
   );
 };
 
--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import AllFoods from '../pages/AllFoods';
 import FoodDetails from '../pages/FoodDetails';
-import Cart from '../pages/Cart';
-import Checkout from '../pages/Checkout';
 import Contact from '../pages/Contact';
 import Login from '../pages/Login';
-import Register from '../pages/Register';
 import Success from '../pages/Success';
+import AuthContext from '../store/auth-context';
+import Admin from '../pages/Admin';
 
 const Routers = () => {
+  const authCtx = useContext(AuthContext);
   return (
     <Routes>
       <Route path='/' element={<Navigate to='/home' />} />
       <Route path='/home' element={<Home />} />
       <Route path='/producten' element={<AllFoods />} />
       <Route path='/producten/:id' element={<FoodDetails />} />
-      <Route path='/cart' element={<Cart />} />
-      <Route path='/checkout' element={<Checkout />} />
+      <Route path='/checkout' element={<FoodDetails />} />
       <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
       <Route path='/contact' element={<Contact />} />
       <Route path='/success' element={<Success />} />
+      <Route
+        path='/admin'
+        element={!authCtx.isLoggedIn ? <Success /> : <Admin />}
+      ></Route>
+      <Route path='*' element={<Navigate to='/home' />} />
     </Routes>
   );
 };
